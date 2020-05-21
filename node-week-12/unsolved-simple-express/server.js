24cd7950ffc4f251051cac0563f821a3ab1ce478
Lucy Yang <8395101kktss@gmail.com>
2020-05-20 20:05:21 -0400

    Added week 15 breakout-session

--- node-week-12/unsolved-simple-express/server.js
+++ node-week-12/unsolved-simple-express/server.js
@@ -3,3 +3,31 @@
     Let's create our Express server!
     Please remember to add comments to help you understand what is happening!
 */
+
+// I am bringing in the Express module in my server.js file in order for me to use.
+const express = require('express')
+
+// Creating a new Express app for server.
+const app = express()
+
+// Creating a variable that will set the port of my server to 5000.
+const PORT = process.env.PORT || 5000
+
+const userdata = require('./public/userdata.json')
+
+// Telling Express app to use expess.static() method in order to stage my client folder.
+app.use(express.static("public"))
+
+// API that will getting all of my users in my userdata.json.
+app.get("/api/users", (req, res) => {
+    if (!userdata) {
+        return res.status(404).send("Resource was not found. Please try again.")
+    }
+
+    res.send(userdata)
+})
+
+// App is running on port 5000
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`)
+})
---
+++ react-week-15/unsolved-hooks-tic-tac-toe/src/Components/ResetButton/Resetbutton.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+// Creating a component that will represent each square on my 
+// reset button.
+const ResetButton = () => {
+
+    return (
+        <>
+            <button className="reset--button">
+                Reset game!
+            </button>
+        </>
+    )
+}
+
+export default ResetButton;
